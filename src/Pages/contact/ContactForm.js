87401f7ef8a21ee0bed82a16d1e90f2a8234d676
brayanmcz <brayan.mcz@gmail.com>
2019-03-10 21:07:29 -0700

    test3

--- src/Pages/contact/ContactForm.js
+++ src/Pages/contact/ContactForm.js
@@ -69,35 +69,11 @@ class ContactForm extends Component {
             <MDBCol md="12">
               <MDBCard>
                 <MDBCardBody>
-                  <form name="contact" netlify action="/contact">
-                    <p>
-                      <label>
-                        Your Name: <input type="text" name="name" />
-                      </label>
-                    </p>
-                    <p>
-                      <label>
-                        Your Email: <input type="email" name="email" />
-                      </label>
-                    </p>
-                    <p>
-                      <label>
-                        Your Role:{" "}
-                        <select name="role[]" multiple>
-                          <option value="leader">Leader</option>
-                          <option value="follower">Follower</option>
-                        </select>
-                      </label>
-                    </p>
-                    <p>
-                      <label>
-                        Message: <textarea name="message" />
-                      </label>
-                    </p>
-                    <p>
-                      <button type="submit">Send</button>
-                    </p>
-                  </form>
+                <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                <input type="text" name="name" />
+                <input type="email" name="email" />
+                <textarea name="message"></textarea>
+              </form>
                   
                 </MDBCardBody>
               </MDBCard>
