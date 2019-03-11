07d77ed9ad0a3b8a8921fbb12b3352686bdaf7a7
brayanmcz <brayan.mcz@gmail.com>
2019-03-10 20:23:15 -0700

    test 1

--- src/Pages/contact/ContactForm.js
+++ src/Pages/contact/ContactForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput, MDBCard, MDBCardBody } from 'mdbreact';
-import styled from 'styled-components';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCard,
+  MDBCardBody
+} from "mdbreact";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   min-height: calc(100vh - 138px - 36px);
@@ -10,87 +19,94 @@ const Wrapper = styled.div`
 `;
 
 class ContactForm extends Component {
+  state = {
+    mobile: null,
+    ios: null
+  };
 
-    state = {
-      mobile: null,
-      ios: null
-    }
-
-    componentDidMount() {
-      var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-      };
-
-      this.setState({
-        mobile: isMobile.any() ? true : false,
-        ios: isMobile.iOS() ? true : false
-      });
-    }
-
-    render() {
+  componentDidMount() {
+    var isMobile = {
+      Android: function() {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
         return (
-          <Wrapper>
-           
-            <MDBContainer>
-              <MDBRow>
-                <MDBCol md="12">
-                <MDBCard>
-                <MDBCardBody>  
-                  <form name="contact" netlify method="POST" netlify-data="true">
-                    <p className="h5 text-center mb-4">Get In Touch !</p>
-                    <div className="grey-text">
-                      <MDBInput
-                        name="name"
-                        label="Name"
-                        icon="user"
-                        group
-                        type="text"
-                        validate
-                        error="wrong"
-                        success="right"
-                      />
-                      <MDBInput
-                        name="message"
-                        type="textarea"
-                        label="Message"
-                        icon="pencil-alt"
-                      />
-                    </div>
-                    <div className="text-center">
-                      <MDBBtn color="pink" name="submit" type="submit">
-                        Send <MDBIcon icon="paper-plane" className="ml-1" />
-                      </MDBBtn>
-                      {
-                      //<a href = "sms://+14158717215/&body=TextHere">IOS</a>
-                      //<a href = "sms://+14158717215/?body=TextHere">Android</a>
-                      }
-                      </div>
+          navigator.userAgent.match(/IEMobile/i) ||
+          navigator.userAgent.match(/WPDesktop/i)
+        );
+      },
+      any: function() {
+        return (
+          isMobile.Android() ||
+          isMobile.BlackBerry() ||
+          isMobile.iOS() ||
+          isMobile.Opera() ||
+          isMobile.Windows()
+        );
+      }
+    };
+
+    this.setState({
+      mobile: isMobile.any() ? true : false,
+      ios: isMobile.iOS() ? true : false
+    });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="12">
+              <MDBCard>
+                <MDBCardBody>
+                  <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                      <label>
+                        Your Name: <input type="text" name="name" />
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Your Email: <input type="email" name="email" />
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Your Role:{" "}
+                        <select name="role[]" multiple>
+                          <option value="leader">Leader</option>
+                          <option value="follower">Follower</option>
+                        </select>
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        Message: <textarea name="message" />
+                      </label>
+                    </p>
+                    <p>
+                      <button type="submit">Send</button>
+                    </p>
                   </form>
-                  </MDBCardBody>
-                  </MDBCard>
-                </MDBCol>
-              </MDBRow>
-            </MDBContainer>
-            </Wrapper>
-          );
-    }
+                  
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </Wrapper>
+    );
+  }
 }
 
 export default ContactForm;
