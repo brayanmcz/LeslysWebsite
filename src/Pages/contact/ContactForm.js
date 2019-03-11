bd8f4365ce31ddd65f01691deea63983c127e962
Brayanmcz <brayan@gmail.com>
2019-03-11 00:58:24 -0700

    test

--- src/Pages/contact/ContactForm.js
+++ src/Pages/contact/ContactForm.js
@@ -66,7 +66,7 @@ class ContactForm extends Component {
             <MDBCol md="12">
               <MDBCard>
                 <MDBCardBody>
-                  <form name="contact" netlify="true" action="/thanks">
+                  <form name="contact" netlify="true">
                     <p>
                       <label>
                         Name <input type="text" name="name" />
