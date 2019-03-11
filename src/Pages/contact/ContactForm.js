e7dd11c3a57202654c396117a674321ceb43b5f0
brayanmcz <brayan.mcz@gmail.com>
2019-03-10 20:14:05 -0700

    changed form handler to netlify

--- src/Pages/contact/ContactForm.js
+++ src/Pages/contact/ContactForm.js
@@ -10,6 +10,40 @@ const Wrapper = styled.div`
 `;
 
 class ContactForm extends Component {
+
+    state = {
+      mobile: null,
+      ios: null
+    }
+
+    componentDidMount() {
+      var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+      };
+
+      this.setState({
+        mobile: isMobile.any() ? true : false,
+        ios: isMobile.iOS() ? true : false
+      });
+    }
+
     render() {
         return (
           <Wrapper>
@@ -19,7 +53,7 @@ class ContactForm extends Component {
                 <MDBCol md="12">
                 <MDBCard>
                 <MDBCardBody>  
-                  <form action="sms:+14158717215?body=TEST" method="POST" accept-charset="UTF-8">
+                  <form netlify method="POST" acceptCharset="UTF-8">
                     <p className="h5 text-center mb-4">Get In Touch !</p>
                     <div className="grey-text">
                       <MDBInput
@@ -28,16 +62,6 @@ class ContactForm extends Component {
                         icon="user"
                         group
                         type="text"
-                        validate
-                        error="wrong"
-                        success="right"
-                      />
-                      <MDBInput
-                        name="email"
-                        label="Email"
-                        icon="envelope"
-                        group
-                        type="email"
                         validate
                         error="wrong"
                         success="right"
@@ -53,7 +77,11 @@ class ContactForm extends Component {
                       <MDBBtn color="pink" name="submit" type="submit">
                         Send <MDBIcon icon="paper-plane" className="ml-1" />
                       </MDBBtn>
-                    </div>
+                      {
+                      //<a href = "sms://+14158717215/&body=TextHere">IOS</a>
+                      //<a href = "sms://+14158717215/?body=TextHere">Android</a>
+                      }
+                      </div>
                   </form>
                   </MDBCardBody>
                   </MDBCard>
